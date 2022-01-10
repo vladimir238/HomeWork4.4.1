4276cbb4cd9e9cc94f78f2d93e72eaaa2247623c
vladimir238 <bokovvladimir1@mail.ru>
2022-01-10 21:02:10 +0300

    Initial commit 1 Initial commit 2

--- src/Number.java
+++ src/Number.java
@@ -3,16 +3,12 @@ public class Number {
     // В данном варианте это две тройки
     public boolean controlPlatinum(String num1) {
 
-        boolean result = false;
-        result = num1.matches("\\+7\\d*3\\d*3\\d*");
+        boolean result;
+        return result = num1.matches("\\+7\\d*3\\d*3\\d*");
 
-        if (result == true) {
-            // System.out.println("3 встречается 2 и более раз " + num1);
-            return true;
-        } else {
-           // System.out.println(" 3 встречается 1 или менее раз " + num1);
-            return false;
-        }
+
+
+
     }
 }
 
